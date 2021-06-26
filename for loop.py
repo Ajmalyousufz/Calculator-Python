5c0a1794d1e6734c56b19f4dab6cc5ad1ce5fecc
Muhammed ajmal y <Ajmalyousufza@gmail.com>
2021-06-26 07:43:48 +0530

    for loop added

---
+++ for loop.py
@@ -0,0 +1,12 @@
+# Here we looking the working of for loop
+
+print("Working of for loop")
+
+skills = ["c", "java", "python", "html", "css", "javaScript", "Bootstrap", "jquery", "android"]
+
+print("\nName : Muhammed Ajmal y\nFullStack Android Developer and Web Developer\n\nSkills :")
+
+for x in skills:
+
+    print(x)
+print(skills)
