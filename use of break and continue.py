b7e1ad9e67bcb1f2433ca48fa9200845f738bc12
Muhammed ajmal y <Ajmalyousufza@gmail.com>
2021-06-26 08:39:09 +0530

    printing 1 to 10 with flow not used break or continue

---
+++ use of break and continue.py
@@ -0,0 +1,8 @@
+# Here we look for the use of break and continue statements
+
+print("\n USE OF BREAK AND CONTINUE STATEMENTS\n")
+
+num = int(input("Enter a number"))
+
+for x in range(num+1):
+    print(x)
