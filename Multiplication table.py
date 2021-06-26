b9c1371bf72c28f97392331d019570ce56aea04f
Muhammed ajmal y <Ajmalyousufza@gmail.com>
2021-06-26 08:22:51 +0530

    multiplication table added

---
+++ Multiplication table.py
@@ -0,0 +1,12 @@
+# Here we create a multiplication table that receives number from user and terminal display the multiplication table of
+# that number till 10
+
+print("\n Multiplication Table\n")
+
+num = int(input("Enter a number\n"))
+
+rang = int(input("Enter table limit\n"))
+
+for x in range(1, rang + 1):
+    result = x * rang
+    print(str(x) + " * " + str(num) + " = " + str(result))
