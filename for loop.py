4efb6d82923e6c731158a30fb2d5afe94b582e76
Muhammed ajmal y <Ajmalyousufza@gmail.com>
2021-06-26 08:09:17 +0530

    String replace method added

--- for loop.py
+++ for loop.py
@@ -2,11 +2,16 @@
 
 print("Working of for loop")
 
-skills = ["c", "java", "python", "html", "css", "javaScript", "Bootstrap", "jquery", "android"]
+skills = ["c.f", "java.f", "python.f", "html.f", "css.f", "javaScript.f", "Bootstrap.f", "jquery.f", "android.f"]
 
 print("\nName : Muhammed Ajmal y\nFullStack Android Developer and Web Developer\n\nSkills :")
 
 for x in skills:
-
     print(x)
 print(skills)
+
+# How excepting last extensions
+
+for x in skills:
+    print(x[0:-2])
+    print(x.replace(".f", ".t"))
